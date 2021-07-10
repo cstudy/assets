9cc22b19f7f6489c6115e7eb395eacef91603fdb
cstudy <29246826+cstudy@users.noreply.github.com>
2021-07-11 02:23:31 +0800

    Update my_wordpress_global.js

--- wordpress/my_wordpress_global.js
+++ wordpress/my_wordpress_global.js
@@ -1,1 +1,5 @@
-window.console.clear();
+window.console.log1=window.console.log;
+window.console.log=function(...args){
+    if((`${args[0]}`).includes("github.com")){return};
+    window.console.log1.apply(null,args);
+}
