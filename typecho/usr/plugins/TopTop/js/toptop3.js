32d3bbec0082b6be25f9d364b92f0732cbe3d10c
cstudy <29246826+cstudy@users.noreply.github.com>
2021-07-30 16:28:42 +0800

    Update and rename toptop2.js to toptop3.js

---
+++ typecho/usr/plugins/TopTop/js/toptop3.js
@@ -0,0 +1,30 @@
+(function(){
+    var hasSetTopTop=false;
+    function setTopTop(event){
+        hasSetTopTop=true;
+        var a = document.getElementsByClassName("back-to-top")[0];
+        function setBackToTop() {
+            if (window.scrollY > 100) {
+                // a.style.visibility = "visible";
+                a.style.right = "20px";
+            } else {
+                // a.style.visibility = "hidden";
+                a.style.right = "-80px";
+            }
+        }
+        setBackToTop();
+        window.addEventListener("scroll", setBackToTop);
+        a.onclick = function () {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        };    
+    }
+    window.addEventListener("DOMContentLoaded", setTopTop);
+    setTimeout(function() {
+        if(!hasSetTopTop){
+            setTopTop();
+        }
+    }, 100);    
+})();
